243ba4947ac7049ef1a061a708c5c970d29240ae
SteffaniCastro <steffani.castro.useche@gmail.com>
2025-10-12 23:41:21 -0400

    fix profile

--- backend/app/Models/UserDetails.php
+++ backend/app/Models/UserDetails.php
@@ -29,7 +29,9 @@ class UserDetails extends Model
         'swish',
         'vat',
         'logo',
-        'img_signature'
+        'img_signature',
+        'personal_phone',
+        'personal_address'
     ];
 
     /**** Relationship ****/
@@ -42,21 +44,23 @@ class UserDetails extends Model
         $userD = UserDetails::updateOrCreate(
             [    'user_id' => $user->id ],
             [
-                'company' => $request->company,
-                'organization_number' => $request->organization_number,
-                'link' => $request->link,
-                'address' => $request->address,
-                'street' => $request->street,
-                'postal_code' => $request->postal_code,
-                'phone' => $request->phone,
-                'bank' => $request->bank,
-                'account_number' => $request->account_number,
+                'company' => $request->company === 'null' ? null : $request->company,
+                'organization_number' => $request->organization_number === 'null' ? null : $request->organization_number,
+                'link' => $request->link === 'null' ? null : $request->link,
+                'address' => $request->address === 'null' ? null : $request->address,
+                'street' => $request->street === 'null' ? null : $request->street,
+                'postal_code' => $request->postal_code === 'null' ? null : $request->postal_code,
+                'phone' => $request->phone === 'null' ? null : $request->phone,
+                'bank' => $request->bank === 'null' ? null : $request->bank,
+                'account_number' => $request->account_number === 'null' ? null : $request->account_number,
                 'iban' => $request->iban === 'null' ? null : $request->iban,
                 'iban_number' => $request->iban_number === 'null' ? null : $request->iban_number,
                 'bic' => $request->bic === 'null' ? null : $request->bic,
                 'plus_spin' => $request->plus_spin === 'null' ? null : $request->plus_spin,
                 'swish' => $request->swish === 'null' ? null : $request->swish,
-                'vat' => $request->vat === 'null' ? null : $request->vat
+                'vat' => $request->vat === 'null' ? null : $request->vat,
+                'personal_phone' => $request->personal_phone === 'null' ? null : $request->personal_phone,
+                'personal_address' => $request->personal_address
             ]
         );
 
