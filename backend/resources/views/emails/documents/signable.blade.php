bf754ce6f73bd85e43ec88979feae656d4222d82
SteffaniCastro <steffani.castro.useche@gmail.com>
2026-02-06 01:50:05 -0400

    fix not authorized

--- backend/resources/views/emails/documents/signable.blade.php
+++ backend/resources/views/emails/documents/signable.blade.php
@@ -10,6 +10,26 @@
                 Vid frågor kring dokumentens innehåll, vänligen kontakta företaget som har skickat dokumentet.
             </p>
         </td>
-    </tr>             
+    </tr>    
+    <tr>
+        <td style="padding:0;margin:0;padding-bottom:15px;padding-top:15px;">
+            <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:19px;color:#454545;font-size:24px; font-weight: 700;">         
+                Uppgifter om avsändande företag
+            </p>
+        </td>
+    </tr>
+    <tr>
+        <td style="padding:0;margin:0;padding-bottom:15px;padding-top:15px;">
+            <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:19px;color:#454545;font-size:16px">
+                <strong>Företagsnamn:</strong> {!! $company->company !!}
+            </p>
+            <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:19px;color:#454545;font-size:16px">
+                <strong>Organisationsnummer:</strong> {!! $company->organization_number !!}
+            </p>
+            <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:19px;color:#454545;font-size:16px">
+                <strong>E-post:</strong> {!! $company->email !!}
+            </p><br>
+        </td>
+    </tr>         
 </table>        
 @endsection
