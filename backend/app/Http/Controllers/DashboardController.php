f077243ae2ca79b82cdbfdf64731c2f1a44543f3
SteffaniCastro <steffani.castro.useche@gmail.com>
2026-03-27 05:57:24 -0400

    fix measures

--- backend/app/Http/Controllers/DashboardController.php
+++ backend/app/Http/Controllers/DashboardController.php
@@ -265,7 +265,7 @@ class DashboardController extends Controller
             $supplierId = $this->getCurrentSupplierId();
             $measures = VehicleTask::query()
                 ->with([
-                    'vehicle:id,reg_num,state_id,model_id,year',
+                    'vehicle:id,reg_num,model_id,year',
                     'vehicle.model:id,name,brand_id',
                     'vehicle.model.brand:id,name'
                 ])
