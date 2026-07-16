742caef9fcb2570e2ca6563118ef6d0960585060
SteffaniCastro <steffani.castro.useche@gmail.com>
2026-07-16 02:21:53 -0400

    Fix Supplier Overview.

--- backend/app/Http/Controllers/SupplierController.php
+++ backend/app/Http/Controllers/SupplierController.php
@@ -961,6 +961,7 @@ class SupplierController extends Controller
                     $filterStart->toDateTimeString(),
                     $filterEnd->toDateTimeString(),
                 ])
+                ->withTrashed()
                 ->clientsCount()
                 ->acceptedSmsCount($filterStart, $filterEnd)
                 ->orderBy('created_at')
