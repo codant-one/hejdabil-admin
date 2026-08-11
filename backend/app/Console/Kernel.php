e68ac63aab64add44ab28f845adec5ef032bf3b8
SteffaniCastro <steffani.castro.useche@gmail.com>
2026-08-11 00:21:26 -0400

    fix kernel

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('supplier:generate-billing')
                  ->daily()
-                 ->at('06:19')
+                 ->at('06:22')
                  ->after(function () {
                     Artisan::call('suppliers:update-plan-date');
                  })
