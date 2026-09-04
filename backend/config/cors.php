81006fef58f105fcb28c4e7690860fe7bce2d17e
SteffaniCastro <steffani.castro.useche@gmail.com>
2026-09-03 22:54:02 -0400

    fix app

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('ALLOWED_CORS_DOMAINS', 'http://localhost:3000')],
+    'allowed_origins' => explode(',', env('ALLOWED_CORS_DOMAINS', 'http://localhost:3000')),
 
     'allowed_origins_patterns' => [],
 
